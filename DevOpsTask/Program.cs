a5e115f5c291e34278a6341329997535c17be539
Prajapati <ramesh.prajapati@accenture.com>
2018-10-30 19:05:44 +0530

    Console application

---
+++ DevOpsTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevOpsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Welcome to DevOps");
+        }
+    }
+}
